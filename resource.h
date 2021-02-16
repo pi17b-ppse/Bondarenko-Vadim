b37cdc4aefa62c5f42b123df75ee01d4a847a660
Lucky Vadim <vadimbond.2000@gmail.com>
2021-02-16 22:26:18 +0300

    в ветку lab3.1(2)

---
+++ resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Script.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_OPTIONS                     102
+#define IDI_ICON_APP                    103
+#define IDB_BITMAP_PANE                 104
+#define IDB_BITMAP_ALPHA                105
+#define IDB_BITMAP_BLOCKS               106
+#define IDB_BITMAP_FILL                 107
+#define IDB_BITMAP_GROUND               108
+#define IDB_BITMAP_NEXTF                109
+#define IDR_TEXT_FIGURES                110
+#define IDR_WAVE_BOOM                   111
+#define IDR_WAVE_ROTATE                 112
+#define IDR_WAVE_STOP                   113
+#define IDC_COMBO_VELOCITY              1000
+#define IDC_CHECK_AUDIO                 1001
+#define IDC_CHECK_REPEAT                1002
+#define IDC_BUTTON_CANCEL               1003
+#define IDC_BUTTON_OK                   1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
